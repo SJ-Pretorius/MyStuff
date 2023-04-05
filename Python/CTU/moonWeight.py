c384ba44a26e48e2589e6f6dbd45271d2879da77
SJ Pretorius <sjpret12345@gmail.com>
2023-04-05 10:59:10 +0200

    Added moonWeight.py

---
+++ Python/CTU/moonWeight.py
@@ -0,0 +1,17 @@
+def displayInstructions():
+    while True:
+        langCode=int(input(f'1 - English or 2 - Espanol >> '))
+        if langCode == 1:
+            ENGLISH_PROMPT = 'Please enter your weight in pounds >> '
+            return ENGLISH_PROMPT
+        elif langCode == 2:
+            SPANISH_PROMPT = "Por favor entre en su peso en libras >> "
+            return SPANISH_PROMPT
+        else:
+            continue
+
+#MOON_FACTOR= 0.166
+#weight=int(input(displayInstructions()))
+#moonWeight = weight * MOON_FACTOR
+#print(f'Your weight on the moon would be {round(moonWeight, 2)} pounds!')
+print(f'Your weight on the moon would be {round(int(input(displayInstructions())) * 0.166, 2)} pounds!')
