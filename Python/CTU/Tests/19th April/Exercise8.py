a4d85c046817056db4fb04e7f4f910036a5340e2
SJ Pretorius <105555756+SJ-Pretorius@users.noreply.github.com>
2023-04-20 08:14:18 +0200

    19 April 2023

---
+++ Python/CTU/Tests/19th April/Exercise8.py
@@ -0,0 +1,8 @@
+num1=int(input('First Number: '))
+num2=int(input('Second Number: '))
+num3=int(input('Third Number: '))
+if num1 == num2 == num3:
+    ans=(num1+num2+num3)*num1
+else:
+    ans=num1+num2+num3
+print(ans)
