b761b4d39e400348967e52e18d8996b0efe8bc20
SJ Pretorius <105555756+SJ-Pretorius@users.noreply.github.com>
2023-05-19 08:22:28 +0200

    Added exercise!

--- Python/CTU/Tests/19th April/Exercise10.py
+++ Python/CTU/Tests/19th April/Exercise10.py
@@ -1,4 +1,2 @@
 import calendar
-theyear=int(input('Input the year: '))
-themonth=int(input('Input the month: '))
-print(calendar.month(theyear, themonth, w=0, l=0))
+print(calendar.month(int(input('Input the year: ')),int(input('Input the month: '))))
